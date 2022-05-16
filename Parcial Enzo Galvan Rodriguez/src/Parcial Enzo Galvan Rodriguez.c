3ae60ebd9276e0b1f44db7701817287dfe0b85e6
enzog-r <enzogalvan.r@gmail.com>
2022-05-16 19:48:45 -0300

    subo parcial

--- Parcial Enzo Galvan Rodriguez/src/Parcial Enzo Galvan Rodriguez.c
+++ Parcial Enzo Galvan Rodriguez/src/Parcial Enzo Galvan Rodriguez.c
@@ -1,17 +1,43 @@
 /*
  ============================================================================
  Name        : Parcial.c
- Author      : 
+ Author      : Enzo Galvan Rodriguez
  Version     :
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
 
+int aplicarDescuento(float precioP);
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+int main() {
+	setbuf(stdout, NULL);
+	float precio = 360;
+
+	printf("%d", aplicarDescuento(precio));
+
+
+
+  return 0;
 }
+
+
+
+int aplicarDescuento(float precioP)
+{
+	int descuento;
+	int resultado;
+
+	descuento = precioP * (float)5 / 100;
+
+	resultado = precioP - descuento;
+
+	return resultado;
+}
+
+
+
